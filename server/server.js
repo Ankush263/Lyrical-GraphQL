eaf46e393e59b5960fd8ed14ede1dda88b15940c
Ankush263 <ankushbanik263@gmail.com>
2024-06-01 18:41:04 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -4,30 +4,34 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const dotenv = require('dotenv');
+
+require('dotenv').config();
 
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = process.env.MONGO_URI;
+
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+	throw new Error('You must provide a Mongo Atlas URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
-  );
+	.once('open', () => console.log('Connected to Mongo Atlas instance.'))
+	.on('error', (error) =>
+		console.log('Error connecting to Mongo Atlas:', error)
+	);
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
-  expressGraphQL({
-    schema,
-    graphiql: true
-  })
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	})
 );
 
 const webpackMiddleware = require('webpack-dev-middleware');

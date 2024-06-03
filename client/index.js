4f705b9b39984521a7f8f14db3f03fa1b6895224
Ankush263 <ankushbanik263@gmail.com>
2024-06-03 17:34:03 +0530

    update

--- client/index.js
+++ client/index.js
@@ -6,8 +6,12 @@ import SongList from './components/SongList';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 import SongCreate from './components/SongCreate';
+import './style/style.css';
+import SongDetail from './components/SongDetail';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+	dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
 	return (
@@ -16,6 +20,7 @@ const Root = () => {
 				<Route path="/" component={App}>
 					<IndexRoute component={SongList} />
 					<Route path="songs/new" component={SongCreate} />
+					<Route path="song/:id" component={SongDetail} />
 				</Route>
 			</Router>
 		</ApolloProvider>
